8574c56373897ac238255878bd1b6065578daa2d
tcyang <tcyang@gm.pu.edu.tw>
2020-11-25 08:59:20 +0800

    合併後，再次修改txv顯示的文字

--- app/src/main/java/tw/edu/pu/csim/tcyang/hello2020/MainActivity.kt
+++ app/src/main/java/tw/edu/pu/csim/tcyang/hello2020/MainActivity.kt
@@ -10,9 +10,11 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         var txv:TextView = findViewById<TextView>(R.id.txv)
-        txv.text = "Hello tcyang"
-        TestDEV()
-        TestMaster()
+        //txv.text = "Hello tcyang"
+        //TestDEV()
+        //TestMaster()
+        txv.setText("嗨，我是楊子青");
+
     }
 
     fun TestMaster() {
