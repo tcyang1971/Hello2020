f0c318e5e3fe7a27073dee8fbc62ae55457ef1af
tcyang <tcyang@gm.pu.edu.tw>
2020-11-25 08:48:57 +0800

    加入master分支程式碼

--- app/src/main/java/tw/edu/pu/csim/tcyang/hello2020/MainActivity.kt
+++ app/src/main/java/tw/edu/pu/csim/tcyang/hello2020/MainActivity.kt
@@ -12,5 +12,11 @@ class MainActivity : AppCompatActivity() {
         var txv:TextView = findViewById<TextView>(R.id.txv)
         txv.text = "Hello tcyang"
 
+        TestMaster()
+    }
+
+    fun TestMaster(){
+        var txv:TextView = findViewById<TextView>(R.id.txv)
+        txv.text = "Master分支"
     }
 }
